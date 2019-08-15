466beb359ecbc749bb7bdfdd9c9f75c27c8e08e8
cocoon45 <tianhao119163@163.com>
2019-08-15 22:58:04 +0800

    add vue old pro

--- controller/article_ctl.go
+++ controller/article_ctl.go
@@ -20,3 +20,20 @@ func (c *Controller) ListArticles(ctx *gin.Context) {
 	paginator := c.App.ArticleSpi.ListArticlesPaginate(1,3)
 	ctx.JSON(200, paginator)
 }
+
+
+// ListArticle godoc
+// @Summary List Article
+// @Description get Articles
+// @Tags article
+// @Accept  json
+// @Produce  json
+// @Success 200 {object} protocol.ArticlePaginator
+// @Failure 400 {object} protocol.Error
+// @Failure 404 {object} protocol.Error
+// @Failure 500 {object} protocol.Error
+// @Router /article [get]
+func (c *Controller) ListArticle(ctx *gin.Context) {
+	paginator := c.App.ArticleSpi.ListArticlesPaginate(1,3)
+	ctx.JSON(200, paginator)
+}
